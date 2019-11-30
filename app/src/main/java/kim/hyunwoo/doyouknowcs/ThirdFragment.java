c211b2f909af4a53a04278b861957c1317ba7e41
heptacode <admin@hyunwoo.kim>
2019-11-30 21:38:10 +0900

    Add AllergyInfo

--- app/src/main/java/kim/hyunwoo/doyouknowcs/ThirdFragment.java
+++ app/src/main/java/kim/hyunwoo/doyouknowcs/ThirdFragment.java
@@ -8,18 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ThirdFragment extends Fragment {
-
+    TextView allergyInfo;
 
     public ThirdFragment() {}
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+        allergyInfo = view.findViewById(R.id.allergyInfo);
+        allergyInfo.setText("1. 난류\n2. 우유\n3. 메밀\n4. 땅콩\n5. 대두\n6. 밀\n7. 고등어\n8. 게\n9. 새우\n10. 돼지고기\n11. 복숭아\n12. 토마토\n13. 아황산염\n14. 호두\n15. 닭고기\n16. 쇠고기\n17. 오징어\n18. 조개류");
+        return view;
     }
 
 }
